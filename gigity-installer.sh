9684b2bbf64a63360333bb1cedd2db641f1c1857
DigitalizedWarfare <christian@digitalizedwarfare.com>
2015-02-03 06:42:27 -0500

    Update gigity-installer.sh

--- gigity-installer.sh
+++ gigity-installer.sh
@@ -79,7 +79,7 @@ show_menus() {
 # Main Menu
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 3] " choice
+	read -p "Enter choice [ 1 - 3 ] " choice
 	case $choice in
 		1) igigity ;;
 		2) install ;;
